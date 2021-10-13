15464d7dc68521bb0e9412c2bb7d7c637a9787c7
samroodAli <devsamrood@gmail.com>
2021-10-13 12:06:25 +0530

    Comments:Create get and comments controllers

--- comments/index.js
+++ comments/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const { json, urlencoded } = require("body-parser");
 const morgan = require("morgan");
+const { randomBytes } = require("crypto");
 
 const app = express();
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.get("/posts/:id/comments", (req, res) => {});
+
+app.post("/posts/:id/comments", (req, res) => {});
 
 const PORT = 5000;
 app.listen(PORT, () => {
