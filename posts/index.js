3d8fcc48f2c15b7c01b303da343da74f35083de8
samroodAli <devsamrood@gmail.com>
2021-10-13 11:51:11 +0530

    Posts:Add additional dependencies morgan and create posts route

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const { randomBytes } = require("crypto");
+const { json, urlencoded } = require("body-parser");
+const morgan = require("morgan");
 
 const app = express();
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
 const posts = {};
 
@@ -8,7 +14,16 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/posts", (req, res) => {});
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+  res.status(201).send(posts[id]);
+});
 
 const PORT = 4000;
 app.listen(PORT, () => {
