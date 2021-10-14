aab6993f654a78ffd3483bbe22a1bb1db76bfc45
samroodAli <devsamrood@gmail.com>
2021-10-14 18:49:49 +0530

    React:Axios create post request to post server but got cors issue

--- client/src/components/PostCreate.jsx
+++ client/src/components/PostCreate.jsx
@@ -3,9 +3,14 @@ import axios from "axios";
 
 const PostCreate = () => {
   const [title, setTitle] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await axios.post("http://localhost:4000/posts", { title });
+    setTitle("");
+  };
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
           <input
