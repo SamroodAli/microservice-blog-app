6d8503607889c355426c553f0f111dc87c99bf66
samroodAli <devsamrood@gmail.com>
2021-10-14 18:53:38 +0530

    Cors:Setup cors in both comments and posts servers

--- comments/index.js
+++ comments/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const { json, urlencoded } = require("body-parser");
 const morgan = require("morgan");
 const { randomBytes } = require("crypto");
+const cors = require('cors')
 
 const app = express();
+
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
+app.use(cors())
 
 const commentsByPostId = {};
 
