9e5325058664a22ad0f383b9d9c987d6170ad5c9
samroodAli <devsamrood@gmail.com>
2021-10-14 19:09:53 +0530

    React:Fetch posts using axios and useEffect

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const PostList = () => {
-  return <div>Post List</div>;
+  const [posts, setPosts] = useState({});
+
+  const fetchPosts = async () => {
+    const { data } = await axios.get("http://localhost:4000/posts");
+    console.log(data);
+    setPosts(data);
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  return <div>posts</div>;
 };
 
 export default PostList;
