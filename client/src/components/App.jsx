a5cc9e2a980f058bce0f5c9b828da02a24443ad3
samroodAli <devsamrood@gmail.com>
2021-10-14 19:02:32 +0530

    Install:And render PostList component in App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,15 @@
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 const App = () => {
   return (
     <div className="container">
-      <h1>Create Post</h1>
+      <h1>Blog App</h1>
+      <h2>Create Post</h2>
       <PostCreate />
+      <hr />
+      <h2>Posts</h2>
+      <PostList />
     </div>
   );
 };
