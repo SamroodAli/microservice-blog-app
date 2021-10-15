5a0675bb54206f3fb07e3298114428d992fddd01
samroodAli <devsamrood@gmail.com>
2021-10-15 10:43:51 +0530

    EventBus:Events create post requests to event bus and to every service

--- event-bus/index.js
+++ event-bus/index.js
@@ -3,8 +3,19 @@ const {json,urlencoded} = require("body-parser")
 const axios = require('axios')
 const morgan = require("morgan")
 
+const PORT=  4005
 const app = express()
 app.use(json())
 app.use(urlencoded({extended:true}))
 app.use(morgan('dev'))
 
+app.post("/events",(req,res)=>{
+  const event = req.body
+  axios.post("http://localhost:4000/events",event)
+  axios.post("http://localhost:4001/events",event)
+  axios.post("http://localhost:4002/events",event)
+})
+
+app.listen(PORT,()=>{
+  console.log(`Event Bus listening on ${PORT}`)
+})
