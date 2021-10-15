1d3cd1d64bcee6382c3d951480f8002c5cf65fcb
samroodAli <devsamrood@gmail.com>
2021-10-15 20:17:48 +0530

    Query:Use switch statement to create posts with comments when events are recieved

--- query/index.js
+++ query/index.js
@@ -1,13 +1,31 @@
-const express = require('express')
-const {json,urlencoded} = require('body-parser')
-const morgan = require("morgan")
+const express = require("express");
+const { json, urlencoded } = require("body-parser");
+const morgan = require("morgan");
 
-const PORT = 4002
-const app = express()
+const PORT = 4002;
+const app = express();
 
-app.use(json())
-app.use(urlencoded({extended:true}))
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
-app.listen(PORT,()=>{
-  console.log(`Query server listening on port ${PORT}`)
-})
+const posts = {};
+
+app.get("/posts", (req, res) => {});
+
+app.post("/events", (req, res) => {
+  const { type, payload } = req.body;
+  switch (type) {
+    case "POST_CREATED":
+      const { id, title } = payload;
+      posts[id] = { id, title, comments: [] };
+    case "COMMENT_CREATED":
+      const { id, postId, content } = payload;
+      const post = posts[id];
+      post.comments.push({ id, content });
+  }
+  res.send({ message: type + " processed" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Query server listening on port ${PORT}`);
+});
