829978570ec52422f3b13279591193a957e2764b
samroodAli <devsamrood@gmail.com>
2021-10-15 20:30:59 +0530

    Fix: Fix response from event bus missing

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,21 +1,22 @@
-const express = require('express')
-const {json,urlencoded} = require("body-parser")
-const axios = require('axios')
-const morgan = require("morgan")
+const express = require("express");
+const { json, urlencoded } = require("body-parser");
+const axios = require("axios");
+const morgan = require("morgan");
 
-const PORT=  4005
-const app = express()
-app.use(json())
-app.use(urlencoded({extended:true}))
-app.use(morgan('dev'))
+const PORT = 4005;
+const app = express();
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-app.post("/events",(req,res)=>{
-  const event = req.body
-  axios.post("http://localhost:4000/events",event)
-  axios.post("http://localhost:4001/events",event)
-  axios.post("http://localhost:4002/events",event)
-})
+app.post("/events", (req, res) => {
+  const event = req.body;
+  axios.post("http://localhost:4000/events", event);
+  axios.post("http://localhost:4001/events", event);
+  axios.post("http://localhost:4002/events", event);
+  res.send({ message: "Event recieved in event Bus" });
+});
 
-app.listen(PORT,()=>{
-  console.log(`Event Bus listening on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Event Bus listening on ${PORT}`);
+});
--- query/index.js
+++ query/index.js
@@ -10,18 +10,25 @@ app.use(urlencoded({ extended: true }));
 
 const posts = {};
 
-app.get("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
 app.post("/events", (req, res) => {
   const { type, payload } = req.body;
+  console.log("I am type " + type, type === "POST_CREATED");
   switch (type) {
-    case "POST_CREATED":
-      const { id, title } = payload;
+    case "POST_CREATED": {
+      console.log("I ran");
+      let { id, title } = payload;
       posts[id] = { id, title, comments: [] };
-    case "COMMENT_CREATED":
-      const { id, postId, content } = payload;
-      const post = posts[id];
+    }
+    case "COMMENT_CREATED": {
+      console.log(" I ran too : {");
+      let { id, postId, content } = payload;
+      let post = posts[postId];
       post.comments.push({ id, content });
+    }
   }
   res.send({ message: type + " processed" });
 });
