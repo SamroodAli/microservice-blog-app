bcdc95c0c21eb60a1ff02e81516bea2d492c0c3a
samroodAli <devsamrood@gmail.com>
2021-10-16 23:38:57 +0530

    Comment:Service update comment event emission to bus

--- comments/index.js
+++ comments/index.js
@@ -38,7 +38,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
   console.log("Event recieved", req.body.type);
   const { type, payload } = req.body;
   switch (type) {
@@ -47,10 +47,14 @@ app.post("/events", (req, res) => {
       const comments = commentsByPostId[postId];
       const comment = comments.find((comment) => comment.id === id);
       comment.status = status;
+      await axios.post("http://localhost:4005/events", {
+        type: "COMMENT_UPDATED",
+        payload: comment,
+      });
       break;
     }
   }
-  res.send({});
+  res.send({ message: `Event ${type} recieved` });
 });
 
 const PORT = 4001;
