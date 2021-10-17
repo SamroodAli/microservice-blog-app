a2b1471a5f132ee85ca7823f8040f54dfecfdcca
samroodAli <devsamrood@gmail.com>
2021-10-17 22:41:50 +0530

    EventStore:Create an array to hold all events

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,6 +9,8 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
   axios.post("http://localhost:4000/events", event);
